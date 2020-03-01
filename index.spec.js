bc931bdc0bddeea35051bec5703879b5758028ad
KenFront <flamma0917@gmail.com>
2020-03-01 23:05:19 +0800

    add no.17

--- index.spec.js
+++ index.spec.js
@@ -7,6 +7,7 @@ require('./problemList/7')
 require('./problemList/10')
 require('./problemList/11')
 require('./problemList/15')
+require('./problemList/17')
 require('./problemList/79')
 
 // rxjs
---
+++ problemList/17.js
@@ -0,0 +1,33 @@
+const digitsMap = {
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z'],
+}
+const letterCombinations = (digits) => {
+  let resultList = []
+  for (let i = 0; i < digits.length; i++) {
+    const charMap = digitsMap[digits[i]]
+    if (i) {
+      resultList = charMap
+        .map((char) => resultList.map((result) => result + char))
+        .reduce((list, items) => [...list, ...items], [])
+    } else {
+      resultList = charMap
+    }
+  }
+  return resultList
+}
+
+describe('leecode:17', () => {
+  test('case 1', () => {
+    const input = '23'
+    const output = ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
+    const result = letterCombinations(input)
+    output.every((item) => expect(result.includes(item)).toBeTruthy())
+  })
+})
