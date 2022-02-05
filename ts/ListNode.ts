35480635b3fc1f7c2920dec409cc7540678b4353
KenFront <flamma0917@gmail.com>
2022-02-05 20:15:43 +0800

    update node version and dependency

--- ts/ListNode.ts
+++ ts/ListNode.ts
@@ -1,5 +1,6 @@
 class ListNode {
   val: number
+  // eslint-disable-next-line no-use-before-define
   next: ListNode | null
   constructor(val?: number, next?: ListNode | null) {
     this.val = val === undefined ? 0 : val
